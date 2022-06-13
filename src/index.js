85e60960711e1d42dad0b56411f7201e8b7eb16c
jacksonohallo <jacksonohallo@gmail.com>
2022-06-13 18:23:20 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createNewTask = document.getElementById("create-task-form");
+
+  const newTaskDescription = document.getElementById("new-task-description");
+
+
+  const listOfTask = document.getElementById("tasks");
+
+  createNewTask.addEventListener("submit", writeNewTask);
+  
 });
+
+const writeNewTask = event => {
+
+  event.preventDefault();
+  //stop form from trying to submit.
+
+
+  const newTaskDeescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDeescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
